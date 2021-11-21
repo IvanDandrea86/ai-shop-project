0c10b3d3a173184bf93a34a89de755449009040b
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-11-21 08:58:53 +0100

    header systemù changed

--- application/source/components/header.php
+++ application/source/components/header.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+  <link rel="icon" type="image/x-icon" href="./media/favicon.svg">
+  <title>AI Shop</title>
+</head>
 <header class="p-3 bg-dark text-white">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -20,8 +35,17 @@
             </form>
 
             <div class="text-end">
+            <?php if (isset($_SESSION['logged-in'])) : ?> 
+                <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="logout.php">Log Out</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="account.php">Profils</a>
+                            </li>   
+            <?php else : ?>
                 <button type="button" class="btn btn-outline-light me-2" data-bs-toggle="modal" data-bs-target="#login_modal">Login</button>
                 <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#signup">Sign-up</button>
+            <?php endif; ?>
             </div>
         </div>
     </div>
